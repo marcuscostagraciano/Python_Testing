8f3e660103caa5745f6c7cec26bcf23bc6aa7cdb
Marcus Vinícius da Costa Graciano <48135737+marcuscostagraciano@users.noreply.github.com>
2022-03-15 16:44:07 -0300

    Update employee_manager.py

--- Class_testing/employee_manager.py
+++ Class_testing/employee_manager.py
@@ -1,13 +1,16 @@
-class Person():
-    def __init__(self, fN, lN, age):
+class Person():    
+    def __init__(self, fN, lN, age = 0):
         self.firstName = fN
         self.lastName = lN
         self.age = age
-        self.email = f"{fN.lower()}.{lN.lower()}@email.com"
 
 
-    def fullName(self):
+    def getFullName(self):
         return f"{self.firstName} {self.lastName}"
+
+
+    def getEmail(self):
+        return f"{self.firstName.lower()}.{self.lastName.lower()}@email.com"
 
 
     def alterAge(self, age) -> int:
@@ -32,14 +35,27 @@ class Manager(Employee):
 
 
     def add_employee(self, emp):
-        if emp not in self.employees: self.employees.append(emp)
-        else: return
+        if emp not in self.employees: self.employees.append(emp)        
 
 
     def remove_employee(self, emp):
         if emp in self.employees: self.employees.remove(emp)
+
+
+    def list_employees(self):
+        for emp in self.employees: print(emp.getFullName())
         
 
-
 emp1 = Employee("Employee","number1",16,1500)
-mng1 = Manager("Manager","number1",18,1600,emp1)
+emp2 = Employee("Employee","number2",18,1700)
+mng1 = Manager("Manager","number1",30,3000)
+
+# Adding employees to test the method: add_employee()
+mng1.add_employee(emp1)
+mng1.add_employee(emp2)
+
+# Removing employees to test the method: remove_employee()
+mng1.remove_employee(emp1)
+mng1.remove_employee(emp2)
+
+mng1.list_employees()
