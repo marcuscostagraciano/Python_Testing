4a31a6db28fb8bb5f57d5b95fb429fba8a681361
Marcus Vinicius da Costa Graciano <marcusviniciusgraciano04@gmail.com>
2023-08-14 15:43:36 -0300

    reformulation (i guess)

--- Seleniun_TestingV1/src/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    import filesAndFoldersHandling  # noqa: F401
-
-
-if __name__ == "__main__":
-    main()
--- euclideanAlgorithm.py
+++
@@ -1,5 +0,0 @@
-def mdc(a = 0, b = 0):
-    while(b != 0):
-        r = a % b
-        a, b = b, r        
-    return a
