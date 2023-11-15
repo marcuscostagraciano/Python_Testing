5839078464429032a39d849ab3f004606f719547
Marcus Vinicius da Costa Graciano <marcusviniciusgraciano04@gmail.com>
2023-11-14 22:31:05 -0300

    data cleaning

--- datawarehouse_and_pdfs/data_management/data_cleaning.py
+++ datawarehouse_and_pdfs/data_management/data_cleaning.py
@@ -6,20 +6,36 @@ def replace_comma(data: str) -> float:
     return float(data.replace(',', '.'))
 
 
+"""
 # Currency
 df = pandas.read_csv('data/dimcurrency.csv', sep=';')
 df.to_csv('data/dimcurrency.csv', index=False)
-"""
-# Special Offer
-df = pandas.read_csv('raw_data/specialoffer.csv', sep=';')
 
+# Date
+df = pandas.read_csv('data/dimdate.csv', sep=';')
+df.to_csv('data/dimdate.csv', index=False)
+
+# Product
+df = pandas.read_csv('data/dimproduct.csv', sep=';')
+df = df.fillna(np.nan).replace([np.nan], [None])
+df.to_csv('data/dimproduct.csv', index=False)
+
+# Sales Reason
+df = pandas.read_csv('data/dimsalesreason.csv', sep=';')
+df.to_csv('data/dimsalesreason.csv', index=False)
+
+# Special Offer
+df = pandas.read_csv('data/dimspecialoffer.csv', sep=';')
 df['discountPercentage'] = (df['discountPercentage'].apply(replace_comma))\
                                 .astype('float64')
-df.to_csv('cleaned_specialoffer.csv', index=False)
+df.to_csv('data/dimspecialoffer.csv', index=False)
 
+# Territory
+df = pandas.read_csv('data/dimterritory.csv', sep=';')
+df.to_csv('data/dimterritory.csv', index=False)
 
 # Fact Sales
-df = pandas.read_csv('raw_data/factsales.csv', sep=';')
+df = pandas.read_csv('data/factsales.csv', sep=';')
 df = df.fillna(np.nan).replace([np.nan], [None])
 
 df['taxes'] = (df['taxes'].apply(replace_comma)).astype('float64')
@@ -29,5 +45,5 @@ df['discounts'] = (df['discounts'].apply(replace_comma)).astype('float64')
 df['discounts'] = df['discounts'] * df['unitPrice']
 df['dateKey'] = df['dateKey'].replace(0, 1)
 
-df.to_csv('cleaned_factsales.csv', index=False)
+df.to_csv('data/factsales.csv', index=False)
 """
