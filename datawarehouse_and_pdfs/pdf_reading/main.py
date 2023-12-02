cf5e241ed23dafda36ab761dc5292e6f6a3e6818
Marcus Vinicius da Costa Graciano <marcusviniciusgraciano04@gmail.com>
2023-12-02 14:05:24 -0300

    start of string cleaning

---
+++ datawarehouse_and_pdfs/pdf_reading/main.py
@@ -0,0 +1,24 @@
+from pypdf import PdfReader
+
+TABLE_SEPARATOR: str = "Indexes"
+
+file_name: str = "modelagem_dw.pdf"
+page_content: str = ""
+tables_list: list[str] = []
+
+with open(f"{file_name}", "rb") as file:
+    reader = PdfReader(file, strict=False)
+    page = reader.pages[0]
+    page_content = page.extract_text()
+
+# print(page_content)
+
+print(len(page_content))
+
+ft_table = page_content[:page_content.find(TABLE_SEPARATOR)]
+# print(ft_table)
+
+page_content = page_content[page_content.find(TABLE_SEPARATOR)+len(TABLE_SEPARATOR):]
+
+print(len(page_content))
+# print(tables_list)
