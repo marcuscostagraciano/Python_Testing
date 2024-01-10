264d41fcf1739b8dca4fc35a8224928424bea7ae
Marcus Vinicius da Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-01-10 00:28:29 -0300

    test(main): rudimentary testing

--- recipe_nutrients_calculator/main.py
+++ recipe_nutrients_calculator/main.py
@@ -8,4 +8,18 @@ oleo_soja = Ingredients('óleo de soja', 52, 432, 0, 48, 0)
 ingredientes = [arroz, oleo_soja, ovos_galinha, ovos_pata]
 torta_arroz_ovos = Recipe(ingredientes)
 
-print(torta_arroz_ovos.total_carbs)
+print(f"total calorias: {torta_arroz_ovos.total_calories}")
+print(f"calorias por grama: {torta_arroz_ovos.total_calories
+      / torta_arroz_ovos.weight: .5f}")
+
+print(f"total proteinas: {torta_arroz_ovos.total_proteins}")
+print(f"proteinas por grama: {torta_arroz_ovos.total_proteins
+      / torta_arroz_ovos.weight: .5f}")
+
+print(f"total gorduras: {torta_arroz_ovos.total_fats}")
+print(f"gorduras por grama: {torta_arroz_ovos.total_fats
+      / torta_arroz_ovos.weight: .5f}")
+
+print(f"total carboidratos: {torta_arroz_ovos.total_carbs}")
+print(f"carboidratos por grama: {torta_arroz_ovos.total_carbs
+      / torta_arroz_ovos.weight: .5f}")
