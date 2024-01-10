f28928bdacfb958451d28f0120267b1aecc7accd
Marcus Vinicius da Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-01-10 00:25:28 -0300

    feat(Ingredients): more properties added  properties

--- recipe_nutrients_calculator/recipe_classes/Ingredients.py
+++ recipe_nutrients_calculator/recipe_classes/Ingredients.py
@@ -9,3 +9,19 @@ class Ingredients:
     proteins: float
     fats: float
     carbs: float
+
+    @property
+    def calories_per_gram(self) -> float:
+        return (self.calories / self.grams)
+
+    @property
+    def proteins_per_gram(self) -> float:
+        return (self.proteins / self.grams)
+
+    @property
+    def fats_per_gram(self) -> float:
+        return (self.fats / self.grams)
+
+    @property
+    def carbs_per_gram(self) -> float:
+        return (self.carbs / self.grams)
