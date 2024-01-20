e4f00c2da94cf2c2387551c838bd35c78250000c
Marcus Vinicius da Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-01-20 12:55:41 -0300

    feat: pdf reading to oop
    scalability using the while loop

--- datawarehouse_and_pdfs/pdf_reading/main.py
+++ datawarehouse_and_pdfs/pdf_reading/main.py
@@ -1,40 +1,38 @@
 from pypdf import PdfReader
 from pydantic import BaseModel
 
+from typing import List
+
 
 class DataBaseTable(BaseModel):
     tbl_name: str
-    columns: list[str]
+    columns: List[str]
+
+
+class DataBaseSchema(BaseModel):
+    # schema_name: str
+    tables: List[DataBaseTable]
 
 
 TABLE_SEPARATOR: str = "Indexes"
 
 file_name: str = r".\pdf_reading\modelagem_dw.pdf"
 page_content: str = ""
-tables_list: list[str] = []
 
 with open(f"{file_name}", "rb") as file:
     reader = PdfReader(file, strict=False)
     page = reader.pages[0]
     page_content = page.extract_text()
 
-# print(page_content)
+schema = DataBaseSchema(tables=[])
 
-# print(len(page_content))
+while len(page_content) > 0:
+    table = page_content[:page_content.find(TABLE_SEPARATOR)].splitlines()
+    schema.tables.append(
+        DataBaseTable(tbl_name=table[0],
+                      columns=table[1:])
+    )
+    page_content = page_content[
+        page_content.find(TABLE_SEPARATOR) + len(TABLE_SEPARATOR) + 1:]
 
-first_table = page_content[:page_content.find(TABLE_SEPARATOR)].split('\n')
-# print(first_table)
-
-
-# print(first_table)
-pydantic_test = DataBaseTable(
-    tbl_name=first_table[0],
-    columns=first_table[1:-1])
-
-print(pydantic_test)
-
-# page_content = page_content[page_content.find(TABLE_SEPARATOR)+len(TABLE_SEPARATOR):]
-
-
-# print(len(page_content))
-# print(tables_list)
+print(schema)
